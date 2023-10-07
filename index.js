0030c3a3a555eff89eee75d7c055e01a41e0c801
joy Kurui <kurui.chepngetich@gmail.com>
2023-10-07 21:49:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers){
+    return ['Antonia', 'Nuru'];
+}
+function returnLastTwoDrivers(drivers){
+    return ['Amari', 'Mo'];
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];   
+
+function createFareMultiplier(num){
+    return function(fare){return num * fare};
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(abc, def){
+return def(abc);
+}
